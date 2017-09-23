e9ffa9338792179a791ae7f66c8f904367f250df
JurrijnP <slubadog@gmail.com>
2017-09-23 15:22:11 +0200

    Testing

--- Scripts/Info.js
+++ Scripts/Info.js
@@ -7,11 +7,11 @@ function Info() {
       {color:#bb29f9}\
       {title:	𝖄𝖔𝖚 𝖍𝖆𝖛𝖊 𝖗𝖊𝖖𝖚𝖊𝖘𝖙𝖊𝖉 𝖞𝖔𝖚𝖗 𝖈𝖎𝖙𝖞 𝖉𝖆𝖙𝖆:}\
       {desc:\
-𝕮𝖆𝖘𝖍: *" + prs(echonia[UserID])["Cash"] + "*\\n\
-𝕭𝖆𝖓𝖐: *" + prs(echonia[UserID])["Bank"] + "*\\n\
-𝕰𝖝𝖕𝖊𝖗𝖎𝖊𝖓𝖈𝖊: *" + prs(echonia[UserID])["Experience"] + "*\\n\
-𝕷𝖊𝖛𝖊𝖑: *" + prs(echonia[UserID])["Level"] + "*\\n\
-𝕮𝖎𝖙𝖞: *" + prs(echonia[UserID])["City"] + "*\\n\
+𝕮𝖆𝖘𝖍: *" + prs(echonia[UserID])["Cash"] + "*\n\
+𝕭𝖆𝖓𝖐: *" + prs(echonia[UserID])["Bank"] + "*\n\
+𝕰𝖝𝖕𝖊𝖗𝖎𝖊𝖓𝖈𝖊: *" + prs(echonia[UserID])["Experience"] + "*\n\
+𝕷𝖊𝖛𝖊𝖑: *" + prs(echonia[UserID])["Level"] + "*\n\
+𝕮𝖎𝖙𝖞: *" + prs(echonia[UserID])["City"] + "*\n\
       }\
     }";
   return Embed;
