c510ccbab7784f8a5e26b95ede21fc22e10f9a9f
JurrijnP <slubadog@gmail.com>
2017-09-23 16:06:23 +0200

    Added Value to reset variable
    Enter "Reset" as function parameter to reset a variable for a user.

--- Scripts/UserObject.js
+++ Scripts/UserObject.js
@@ -9,31 +9,43 @@ function UserObject(CurrentObject, City, Cash, Bank, Experience, Level, Game) {
 	};
   if (City !== "") {
     obj["City"] = City;
+  } else if (City === "Reset") {
+    obj["City"] = "Echonia Castle";
   } else {
     obj["City"] = prs(CurrentObject)["City"];
   }
   if (Cash !== "") {
     obj["Cash"] = Cash;
+  } else if (Cash === "Reset") {
+    obj["Cash"] = 50;
   } else {
     obj["Cash"] = prs(CurrentObject)["Cash"];
   }
   if (Bank !== "") {
     obj["Bank"] = Bank;
+  } else if (Bank === "Reset") {
+    obj["Bank"] = 0;
   } else {
     obj["Bank"] = prs(CurrentObject)["Bank"];
   }
   if (Experience !== "") {
     obj["Experience"] = City;
+  } else if (Experience === "Reset") {
+    obj["Experience"] = 0;
   } else {
     obj["Experience"] = prs(CurrentObject)["Experience"];
   }
   if (Level !== "") {
     obj["Level"] = Level;
+  } else if (Level === "Reset") {
+    obj["Level"] = 0;
   } else {
     obj["Level"] = prs(CurrentObject)["Level"];
   }
   if (City !== "") {
     obj["Game"] = City;
+  } else if (Game === "Reset") {
+    obj["Game"] = "";
   } else {
     obj["Game"] = prs(CurrentObject)["Game"];
   }
