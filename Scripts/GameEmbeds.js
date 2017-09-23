ca9cf5e08d36f9a4a41ea5f7a89fdb15fc53326c
JurrijnP <slubadog@gmail.com>
2017-09-23 21:34:45 +0200

    Added GameEmbeds Script

---
+++ Scripts/GameEmbeds.js
@@ -0,0 +1,37 @@
+function UpdateEmbed(Game) {
+	var GameEmbeds = {
+		"Battleships": {
+			"Message": 361225544043593738,
+			"Color": "#CC0000",
+			"Banner": "Battleship.jpg"
+		},
+		"Hangman": {
+			"Message": 361225761274724354,
+			"Color": "#0000BB",
+			"Banner": "Hangman.jpg"
+		},
+		"Yahtzee": {
+			"Message": 361227727514107917,
+			"Color": "#00DD00",
+			"Banner": "Yahtzee.png"
+		},
+		"Poker": {
+			"Message": 361228194331754507,
+			"Color": "#DD00DD",
+			"Banner": "Poker.jpg"
+		}
+	};
+	return var Message = "{edit:" + GameEmbeds[Game]["Message"] + "}\n{embed:\
+{type:rich}\
+{color:" + GameEmbeds[Game]["Color"] + "}\
+{title:" + Game + "}\
+{desc:\
+Game made: " + prs(MainData[Game])["Game"] + "\n\
+Started: " + prs(MainData[Game])["Started"] + "\n\
+Players: " + prs(MainData[Game])["Players"] + "\n\
+Max Players: " + prs(MainData[Game])["Max"] + "\n\
+Under Development: " + prs(MainData[Game])["DevMode"] + "\
+}\
+{image|url:https://raw.githubusercontent.com/JurrijnP/Echonia/master/Game%20Banners/" + GameEmbeds[Game]["Banner"] + "}\
+}";
+}
