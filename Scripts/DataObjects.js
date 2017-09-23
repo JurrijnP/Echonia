924bd0aa3373e92f3526ae7fe13a44f59690f86a
JurrijnP <slubadog@gmail.com>
2017-09-23 19:58:14 +0200

    New Script & Snippet Update

---
+++ Scripts/DataObjects.js
@@ -0,0 +1,63 @@
+function DataObjects(Game, Variable, Value) {
+	var Options = {
+		"0": "DevMode",
+		"1": "Temp",
+		"2": "Game",
+		"3": "Started",
+		"4": "Players",
+		"5": "Max"
+	};
+	// Check if the correct variable type gets entered.
+	if ((Variable === "0") || (Variable === "2") || (Variable === "3")) {
+		if ((typeof prs(Value)) !== "boolean") {
+			return resp = "{redirect:296318607112142848}`" + Value + "` is type of *" + (typeof prs(Value)) + "*, Expected type of *Boolean*.";
+		}
+	}
+	if ((Variable === "4") || (Variable === "5")) {
+		if ((typeof prs(Value)) !== "number") {
+			return resp = "{redirect:296318607112142848}`" + Value + "` is type of *" + (typeof prs(Value)) + "*, Expected type of *Number*.";
+		}
+	}
+	if (Variable === "1") {
+		if ((typeof Value) !== "string") {
+			return resp = "{redirect:296318607112142848}`" + Value + "` is type of *" + (typeof Value) + "*, Expected type of *String*.";
+		}
+	}
+	var Objects = {
+		"Hangman": {
+			"DevMode": false,
+			"Temp": "",
+			"Game": false,
+			"Started": false,
+			"Players": 0,
+			"Max": 8
+		},
+		"Yahtzee": {
+			"DevMode": false,
+			"Temp": "",
+			"Game": false,
+			"Started": false,
+			"Players": 0,
+			"Max": 8
+		},
+		"Battleships": {
+			"DevMode": false,
+			"Temp": "",
+			"Game": false,
+			"Started": false,
+			"Players": 0,
+			"Max": 2
+		},
+		"Slotmachine": {
+			"DevMode": false
+		},
+		"Jackpot": {
+			"Lucky Number": 5157,
+			"Enabled": []
+		}
+	};
+	var obj = Objects[Game];
+	obj[(Options[Variable])] = Value;
+	obj = str(obj);
+	return obj;
+}
