2b284e2c6c1c4654b95a6fb96297b50ea7c2c6f3
JurrijnP <slubadog@gmail.com>
2017-09-25 18:58:15 +0200

    Debugging

--- Scripts/DataObjects.js
+++ Scripts/DataObjects.js
@@ -72,7 +72,7 @@ function DataObjects(Game, Variable, Value) {
 			obj[(Options[i])] = prs(MainData[Game])[(Options[i])];
 		};
 		if (Value === "Reset") {
-			resp = "```" + Game + "\n" + Variable + "\n" + Value + "```\n";
+			resp = "```Game: " + Game + "\nVariable: " + Variable + "\nValue: " + Value + "```\n";
 			obj = str(obj);
 			return obj;
 		} else {
