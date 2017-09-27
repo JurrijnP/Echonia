29b4910a92bf5130caad27a5ff8eae0f607ce8a2
JurrijnP <slubadog@gmail.com>
2017-09-27 16:37:06 +0200

    Help Update

--- Scripts/Games.js
+++ Scripts/Games.js
@@ -42,5 +42,28 @@ function Games(Game, Variable, Value) {
 };
 
 function GameHelp(Game, PlayerType) {
-
+	if (Game === "Hangman") {
+		if (PlayerType === 0) {
+			resp = "\
+{pm}\
+Hey " + Username + " you are the person to choose the word!\
+To invite other people you must first do 2 other things:\
+**1.** You have to set a word by typing `hm?setword <Word>`\
+For the worldrules type `hm?wordrules`\
+**2.** You have to set a range of the minimum & maximum amount of gold that can be used to bet.\
+For more help bet ranging type `e?help Betting Range` in <#290983769840680961>\
+Once you have done the above 2 things you can start inviting people by typing `e?invite Hangman <Mention user here>` in <#290983769840680961>\
+The user you invited will get a PM with a special code to accept or cancel your invite.\
+You can invite up to 7 players.\
+Once you have invited everybody you want to invite everybody has to place a bet in the range you set.\
+When everybody has set a bet you can start the game!\
+You can type `hm?start` and a random order will be created in which\
+each player will get a turn to guess a letter.\
+once the game ends the player that guessed the word will get 90% of the gold from all bets.\
+The other 10% will go into the jackpot which you have chance of winning when playing games!\
+";
+		} else if (PlayerType === 1) {
+			
+		}
+	}
 };
