89a35f0cec29a4059c536d126ace039adad5f694
JurrijnP <slubadog@gmail.com>
2017-09-27 11:39:23 +0200

    New code for Hangman
    Work in Progress.

--- Scripts/Games.js
+++ Scripts/Games.js
@@ -1,3 +1,40 @@
+use Battleships;
+use Hangman;
+use Poker;
+use Yahtzee;
+use MainData;
+import "https://raw.githubusercontent.com/JurrijnP/Echonia/master/Scripts/VariableManipulation.js";
+import "https://raw.githubusercontent.com/JurrijnP/Echonia/master/Scripts/DataObjects.js";
+
+
 function Games(Game, Variable, Value) {
+	/*
+		Variable meanings:
+			0 = GameCreation
+			1 = PlayerAdd
+			2 = GameStart
+			3 = GameUpdate
+			4 = GameEnd
+	*/
+	if (Game === "Hangman") {
+		switch(Variable) {
+			case 0:
+				Hangman["Chooser"] = Value;
+				Hangman["Word"] = "";
+				Hangman["Letters"] = str([]);
+				Hangman["Guessers"] = str([]);
+				Hangman["Mistakes"] = 0;
+			case 1:
+				DataObjects(Confirmation, 4, (prs(MainData["Hangman"])["Players"] + 1));
+				var arr = prs(Hangman["Guessers"]);
+				arr[(arr.length)] = Value;
+				Hangman["Guessers"] = str(arr);
+			default:
+				resp += "ProcessError: Case was not selected.";
+		}
+	}
+};
+
+function GameHelp(Game, PlayerType) {
 
 };
