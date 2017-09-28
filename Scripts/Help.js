4884574bf9493b4dbfbefb8a5fefb15ec35a89c8
JurrijnP <slubadog@gmail.com>
2017-09-28 16:57:31 +0200

    Test

--- Scripts/Help.js
+++ Scripts/Help.js
@@ -8,14 +8,13 @@ function Help() {
 	if (arguments.length === 0) {
 		msg = "{del}\n*" + Username + "* haven't given me a topic and subtopic to give you more information about.\nType `e?help Topics` to get a list of all topics.";
 	} else if (arguments.length === 1 && arguments[0] === "Topics") {
-		msg = "{del}\n{pm}\n\
-{embed:\
-	{type:rich}\
-	{Title:Help Topics}\
-	{field[0]|title:Betting}\
-	{field[0]|value:Subtopics: Range}\
-	{field[0]|inline:false}\
-	{footer|icon:https://raw.githubusercontent.com/JurrijnP/Echonia/master/Images/Echonia.png}\
+		msg = "{del}\n{pm}\n{embed:\n\
+	{type:rich}\n\
+	{Title:Help Topics}\n\
+	{field[0]|title:Betting}\n\
+	{field[0]|value:Subtopics: Range}\n\
+	{field[0]|inline:false}\n\
+	{footer|icon:https://raw.githubusercontent.com/JurrijnP/Echonia/master/Images/Echonia.png}\n\
 }";
 	}
 	return msg;
