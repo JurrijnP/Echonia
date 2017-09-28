14a29e09744f650516f72a86bf7727ad1d8b90b7
JurrijnP <slubadog@gmail.com>
2017-09-28 14:00:24 +0200

    Test

--- Scripts/Help.js
+++ Scripts/Help.js
@@ -1,6 +1,6 @@
 function Help() {
 	if (arguments.length === 0) {
-		resp = "{del}\n*" + Username "* haven't given me a topic and subtopic to give you more information about.\nType `e?help Topics` to get a list of all topics.";
+		resp = "{del}\n*" + Username + "* haven't given me a topic and subtopic to give you more information about.\nType `e?help Topics` to get a list of all topics.";
 		return;
 	if (arguments.length === 1 && arguments[0] === "Topics") {
 		resp = "{del}\n{pm}\n\
