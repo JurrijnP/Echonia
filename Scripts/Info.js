ab0f2a99876c20885fcda30f40b8f852a9c8aba6
JurrijnP <slubadog@gmail.com>
2017-10-02 17:19:16 +0200

    Update

--- Scripts/Info.js
+++ Scripts/Info.js
@@ -1,17 +1,17 @@
 function Info() {
-  use echonia;
+  use Echonia;
   var Embed = "";
-  if (echonia[UserID]) {
+  if (Echonia[UserID]) {
     Embed = "{embed:\
       {type:rich}\
       {color:#bb29f9}\
       {title:	𝖄𝖔𝖚 𝖍𝖆𝖛𝖊 𝖗𝖊𝖖𝖚𝖊𝖘𝖙𝖊𝖉 𝖞𝖔𝖚𝖗 𝖈𝖎𝖙𝖞 𝖉𝖆𝖙𝖆:}\
       {desc:\
-𝕮𝖆𝖘𝖍: *" + prs(echonia[UserID])["Cash"] + "*\n\
-𝕭𝖆𝖓𝖐: *" + prs(echonia[UserID])["Bank"] + "*\n\
-𝕰𝖝𝖕𝖊𝖗𝖎𝖊𝖓𝖈𝖊: *" + prs(echonia[UserID])["Experience"] + "*\n\
-𝕷𝖊𝖛𝖊𝖑: *" + prs(echonia[UserID])["Level"] + "*\n\
-𝕮𝖎𝖙𝖞: *" + prs(echonia[UserID])["City"] + "*\n\
+𝕮𝖆𝖘𝖍: *" + prs(Echonia[UserID])["Cash"] + "*\n\
+𝕭𝖆𝖓𝖐: *" + prs(Echonia[UserID])["Bank"] + "*\n\
+𝕰𝖝𝖕𝖊𝖗𝖎𝖊𝖓𝖈𝖊: *" + prs(Echonia[UserID])["Experience"] + "*\n\
+𝕷𝖊𝖛𝖊𝖑: *" + prs(Echonia[UserID])["Level"] + "*\n\
+𝕮𝖎𝖙𝖞: *" + prs(Echonia[UserID])["City"] + "*\n\
       }\
     }";
   return Embed;
